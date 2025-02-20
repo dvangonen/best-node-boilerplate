2b20b2f676e7a5c78536af411c61d500440a64be
dvangonen <93822098+dvangonen@users.noreply.github.com>
2025-02-21 01:13:59 +0300

    update package.json scripts, add public package.json, and modify index.ts to log API key

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { App } from '@/app/app';
 
 function main(a: string = 'Hello World!') {
 	console.log(a);
-	console.log('Hello World 2!');
+	console.log('Hello World 2!', process.env.MY_API_KEY);
 
 	App();
 }
